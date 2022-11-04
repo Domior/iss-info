e7d86b0710141db9e573d312c9ad3c95b8281952
Andrew Snisar <andrew.snisar@gmail.com>
2022-11-04 18:28:00 +0200

    tried with google maps
    marker doesnt display, idk why, no errors

--- src/components/Location/Map/index.jsx
+++ src/components/Location/Map/index.jsx
@@ -1,27 +1,51 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
-import styles from './Map.module.scss';
+import { GOOGLE_MAPS_API_KEY } from '../../../constants/api';
+
+// import styles from './Map.module.scss';
 
 const Map = () => {
   const { coordinates } = useSelector(state => state.location);
 
+  const mapContainerStyle = {
+    width: '100%',
+    height: '530px',
+  };
+
+  const position = {
+    lat: coordinates[0],
+    lng: coordinates[1],
+  };
+
   return (
-    <MapContainer
-      center={coordinates}
-      zoom={1}
-      scrollWheelZoom={false}
-      className={styles.mapContainer}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={coordinates}>
-        <Popup>Current location of the ISS in space</Popup>
-      </Marker>
-    </MapContainer>
+    <div style={{ height: '100vh', width: '100%' }}>
+      <LoadScript googleMapsApiKey={GOOGLE_MAPS_API_KEY}>
+        <GoogleMap mapContainerStyle={mapContainerStyle} zoom={2} center={position}>
+          <Marker
+            icon={'https://www.svgrepo.com/show/42083/satellite.svg'}
+            position={position}
+          />
+        </GoogleMap>
+      </LoadScript>
+    </div>
+
+    // <MapContainer
+    //   center={coordinates}
+    //   zoom={1}
+    //   scrollWheelZoom={false}
+    //   className={styles.mapContainer}
+    // >
+    //   <TileLayer
+    //     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    //     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    //   />
+    //   <Marker position={coordinates}>
+    //     <Popup>Current location of the ISS in space</Popup>
+    //   </Marker>
+    // </MapContainer>
   );
 };
 
--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,4 @@
 export const API_ISS_CREW = `http://api.open-notify.org/astros.json`;
 export const API_ISS_LOCATION = `http://api.open-notify.org/iss-now.json`;
 export const API_PHOTO = `https://joeschmoe.io/api/v1`;
+export const GOOGLE_MAPS_API_KEY = 'AIzaSyChji3ym0qw7Hg9unLste51NpixjethzHo';
